0043cdbd49739608ddfba60792be7ca21de0addc
reva102 <143382570+reva102@users.noreply.github.com>
2023-11-09 17:06:05 +0530

    Update mongoose.js

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,13 +1,20 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://127.0.0.1/Placement_Cell');
+const DB = 'mongodb+srv://revanthr227:gg44X8T8OomHfViI@placementcell.bqssyxh.mongodb.net/?retryWrites=true&w=majority';
 
-const db =  mongoose.connection;
 
-db.on('error',console.log.bind(console,"Error connecting the mongodb"));
 
-db.once('open',function(){
-    console.log('connected to database: Mongodb');
-})
+mongoose.connect(DB, {
+	useNewUrlParser: true,
+	useUnifiedTopology: true,
+});
 
-module.exports =db;
+const db = mongoose.connection;
+
+db.on('error', console.error.bind(console, 'Error in connecting to MongoDB'));
+
+db.once('open', function () {
+	console.log('Connected to Database :: Mongodb');
+});
+
+module.exports = mongoose;
